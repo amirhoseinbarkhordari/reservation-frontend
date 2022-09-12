c8016b3a8a20980b94ddc541892186809734c4f8
Farbod Shams <farbodshams.2000@gmail.com>
2022-09-12 16:08:54 +0430

    Added video tutorial component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,27 @@
-import type { Theme } from '@mui/material';
-import type { NextPage } from 'next';
-import { Container, Divider, useMediaQuery, useTheme } from '@mui/material';
+import type {Theme} from '@mui/material';
+import type {NextPage} from 'next';
+import {Container, Divider, useMediaQuery} from '@mui/material';
 import AboutProject from '../modules/aboutProject/components/AboutProject';
 import Header from '../modules/header/components/Header';
 import PurchaseTicket from '../modules/purchaseTicket/components/PurchaseTicket';
-import { TicketTypes } from '../modules/shared/components/TicketTypes';
+import {TicketTypes} from '../modules/shared/components/TicketTypes';
+import TutorialVideo from "../modules/Tutorial/components/TutorialVideo";
 
 const Home: NextPage = () => {
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
-  return (
-    <>
-      <Header />
-      <Container>
-        <Divider sx={{ width: "100%" }} />
-        <AboutProject ticketTypes={TicketTypes} isMobile={isMobile} />
-        <PurchaseTicket ticketTypes={TicketTypes} isMobile={isMobile} />
-      </Container>
+    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
+    return (
+        <>
+            <Header/>
+            <Container>
+                <Divider sx={{width: "100%"}}/>
+                <AboutProject ticketTypes={TicketTypes} isMobile={isMobile}/>
+                <Divider sx={{width: "100%"}}/>
+                <TutorialVideo/>
+                <PurchaseTicket ticketTypes={TicketTypes} isMobile={isMobile}/>
+            </Container>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default Home
