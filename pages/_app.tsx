4115c2c94e571527c9f04574c5b816bb54e7ed02
Farbod Shams <farbodshams.2000@gmail.com>
2022-09-14 19:42:27 +0430

    [MERGE UNSAFE] Added en locale, (not completed yet)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,27 @@
-import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import type {AppProps} from 'next/app'
+import {CssBaseline, ThemeProvider} from "@mui/material";
 import DefaultTheme from "../modules/_app/theme/DefaultTheme";
 import "../modules/_app/assets/stylesheet/fonts.css";
 import LocaleDirectionSetter from "../modules/l10n/components/LocaleDirectionSetter";
 import {useRouter} from "next/router";
+import {NextIntlProvider} from 'next-intl';
 
 
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
     const router = useRouter();
 
     return (
-    <>
-        <LocaleDirectionSetter>
-            <ThemeProvider theme={DefaultTheme(router.locale ?? "en")}>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </LocaleDirectionSetter>
-    </>
-  );
+        <>
+            <LocaleDirectionSetter>
+                <NextIntlProvider messages={pageProps.messages}>
+                    <ThemeProvider theme={DefaultTheme(router.locale ?? "en")}>
+                        <CssBaseline/>
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </NextIntlProvider>
+            </LocaleDirectionSetter>
+        </>
+    );
 }
 
 
